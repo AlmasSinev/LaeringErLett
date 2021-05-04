d6dc9464030551ad19be804c850d4907037031e3
Алмас Синев <26almassinev@gmail.com>
2021-05-04 21:58:30 +0300

    init commit

---
+++ src/main/java/com/iildarado/laeringerlett/LaeringErLettApplication.java
@@ -0,0 +1,13 @@
+package com.iildarado.laeringerlett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaeringErLettApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaeringErLettApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iildarado/laeringerlett/LaeringErLettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iildarado.laeringerlett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaeringErLettApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
